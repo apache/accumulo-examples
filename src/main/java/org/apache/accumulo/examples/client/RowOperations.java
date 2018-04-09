aad6980168996deb0bb846f4a9bb9409f2e2af35
Mike Walch <mwalch@apache.org>
2018-04-09 15:22:57 -0400

    Fixes #16 - Refactored examples in client.md

--- src/main/java/org/apache/accumulo/examples/client/RowOperations.java
+++ src/main/java/org/apache/accumulo/examples/client/RowOperations.java
@@ -16,8 +16,6 @@
  */
 package org.apache.accumulo.examples.client;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 import java.util.Map.Entry;
 
 import org.apache.accumulo.core.client.AccumuloException;
@@ -33,10 +31,6 @@ import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
-import org.apache.accumulo.examples.cli.BatchWriterOpts;
-import org.apache.accumulo.examples.cli.ClientOpts;
-import org.apache.accumulo.examples.cli.ScannerOpts;
-import org.apache.hadoop.io.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,169 +41,88 @@ public class RowOperations {
 
   private static final Logger log = LoggerFactory.getLogger(RowOperations.class);
 
-  private static Connector connector;
-  private static String tableName = "example";
-  private static BatchWriter bw;
+  private static void printAll(Connector connector) throws TableNotFoundException {
+    try (Scanner scanner = connector.createScanner("rowops", Authorizations.EMPTY)) {
+      for (Entry<Key,Value> entry : scanner) {
+        log.info("Key: " + entry.getKey().toString() + " Value: " + entry.getValue().toString());
+      }
+    }
+  }
 
-  public static void main(String[] args) throws AccumuloException, AccumuloSecurityException, TableExistsException, TableNotFoundException,
-      MutationsRejectedException {
+  private static void printRow(String row, Connector connector) throws TableNotFoundException {
+    try (Scanner scanner = connector.createScanner("rowops", Authorizations.EMPTY)) {
+      scanner.setRange(Range.exact(row));
+      for (Entry<Key,Value> entry : scanner) {
+        log.info("Key: " + entry.getKey().toString() + " Value: " + entry.getValue().toString());
+      }
+    }
+  }
 
-    ClientOpts opts = new ClientOpts();
-    ScannerOpts scanOpts = new ScannerOpts();
-    BatchWriterOpts bwOpts = new BatchWriterOpts();
-    opts.parseArgs(RowOperations.class.getName(), args, scanOpts, bwOpts);
+  private static void deleteRow(String row, Connector connector, BatchWriter bw) throws MutationsRejectedException, TableNotFoundException {
+    Mutation mut = new Mutation(row);
+    try (Scanner scanner = connector.createScanner("rowops", Authorizations.EMPTY)) {
+      scanner.setRange(Range.exact(row));
+      for (Entry<Key,Value> entry : scanner) {
+        mut.putDelete(entry.getKey().getColumnFamily(), entry.getKey().getColumnQualifier());
+      }
+    }
+    bw.addMutation(mut);
+    bw.flush();
+  }
 
-    // First the setup work
-    connector = opts.getConnector();
-
-    // lets create an example table
-    connector.tableOperations().create(tableName);
+  public static void main(String[] args) throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
+    Connector connector = Connector.builder().usingProperties("conf/accumulo-client.properties").build();
+    try {
+      connector.tableOperations().create("rowops");
+    } catch (TableExistsException e) {
+      // ignore
+    }
 
     // lets create 3 rows of information
-    Text row1 = new Text("row1");
-    Text row2 = new Text("row2");
-    Text row3 = new Text("row3");
+    Mutation mut1 = new Mutation("row1");
+    Mutation mut2 = new Mutation("row2");
+    Mutation mut3 = new Mutation("row3");
 
-    // Which means 3 different mutations
-    Mutation mut1 = new Mutation(row1);
-    Mutation mut2 = new Mutation(row2);
-    Mutation mut3 = new Mutation(row3);
+    mut1.put("col", "1", "v1");
+    mut1.put("col", "2", "v2");
+    mut1.put("col", "3", "v3");
 
-    // And we'll put 4 columns in each row
-    Text col1 = new Text("1");
-    Text col2 = new Text("2");
-    Text col3 = new Text("3");
-    Text col4 = new Text("4");
+    mut2.put("col", "1", "v1");
+    mut2.put("col", "2", "v2");
+    mut2.put("col", "3", "v3");
 
-    // Now we'll add them to the mutations
-    mut1.put(new Text("column"), col1, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-    mut1.put(new Text("column"), col2, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-    mut1.put(new Text("column"), col3, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-    mut1.put(new Text("column"), col4, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-
-    mut2.put(new Text("column"), col1, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-    mut2.put(new Text("column"), col2, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-    mut2.put(new Text("column"), col3, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-    mut2.put(new Text("column"), col4, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-
-    mut3.put(new Text("column"), col1, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-    mut3.put(new Text("column"), col2, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-    mut3.put(new Text("column"), col3, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
-    mut3.put(new Text("column"), col4, System.currentTimeMillis(), new Value("This is the value for this key".getBytes(UTF_8)));
+    mut3.put("col", "1", "v1");
+    mut3.put("col", "2", "v2");
+    mut3.put("col", "3", "v3");
 
     // Now we'll make a Batch Writer
-    bw = connector.createBatchWriter(tableName, bwOpts.getBatchWriterConfig());
+    try (BatchWriter bw = connector.createBatchWriter("rowops")) {
 
-    // And add the mutations
-    bw.addMutation(mut1);
-    bw.addMutation(mut2);
-    bw.addMutation(mut3);
+      // And add the mutations
+      bw.addMutation(mut1);
+      bw.addMutation(mut2);
+      bw.addMutation(mut3);
 
-    // Force a send
-    bw.flush();
+      // Force a send
+      bw.flush();
 
-    // Now lets look at the rows
-    Scanner rowThree = getRow(scanOpts, new Text("row3"));
-    Scanner rowTwo = getRow(scanOpts, new Text("row2"));
-    Scanner rowOne = getRow(scanOpts, new Text("row1"));
+      log.info("This is only row2");
+      printRow("row2", connector);
 
-    // And print them
-    log.info("This is everything");
-    printRow(rowOne);
-    printRow(rowTwo);
-    printRow(rowThree);
-    System.out.flush();
+      log.info("This is everything");
+      printAll(connector);
 
-    // Now lets delete rowTwo with the iterator
-    rowTwo = getRow(scanOpts, new Text("row2"));
-    deleteRow(rowTwo);
+      deleteRow("row2", connector, bw);
 
-    // Now lets look at the rows again
-    rowThree = getRow(scanOpts, new Text("row3"));
-    rowTwo = getRow(scanOpts, new Text("row2"));
-    rowOne = getRow(scanOpts, new Text("row1"));
+      log.info("This is row1 and row3");
+      printAll(connector);
 
-    // And print them
-    log.info("This is row1 and row3");
-    printRow(rowOne);
-    printRow(rowTwo);
-    printRow(rowThree);
-    System.out.flush();
+      deleteRow("row1", connector, bw);
+   }
 
-    // Should only see the two rows
-    // Now lets delete rowOne without passing in the iterator
-
-    deleteRow(scanOpts, row1);
-
-    // Now lets look at the rows one last time
-    rowThree = getRow(scanOpts, new Text("row3"));
-    rowTwo = getRow(scanOpts, new Text("row2"));
-    rowOne = getRow(scanOpts, new Text("row1"));
-
-    // And print them
     log.info("This is just row3");
-    printRow(rowOne);
-    printRow(rowTwo);
-    printRow(rowThree);
-    System.out.flush();
+    printAll(connector);
 
-    // Should only see rowThree
-
-    // Always close your batchwriter
-
-    bw.close();
-
-    // and lets clean up our mess
-    connector.tableOperations().delete(tableName);
-
-    // fin~
-
+    connector.tableOperations().delete("rowops");
   }
-
-  /**
-   * Deletes a row given a text object
-   */
-  private static void deleteRow(ScannerOpts scanOpts, Text row) throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
-    deleteRow(getRow(scanOpts, row));
-  }
-
-  /**
-   * Deletes a row, given a Scanner of JUST that row
-   */
-  private static void deleteRow(Scanner scanner) throws MutationsRejectedException {
-    Mutation deleter = null;
-    // iterate through the keys
-    for (Entry<Key,Value> entry : scanner) {
-      // create a mutation for the row
-      if (deleter == null)
-        deleter = new Mutation(entry.getKey().getRow());
-      // the remove function adds the key with the delete flag set to true
-      deleter.putDelete(entry.getKey().getColumnFamily(), entry.getKey().getColumnQualifier());
-    }
-    bw.addMutation(deleter);
-    bw.flush();
-  }
-
-  /**
-   * Just a generic print function given an iterator. Not necessarily just for printing a single row
-   */
-  private static void printRow(Scanner scanner) {
-    // iterates through and prints
-    for (Entry<Key,Value> entry : scanner)
-      log.info("Key: " + entry.getKey().toString() + " Value: " + entry.getValue().toString());
-  }
-
-  /**
-   * Gets a scanner over one row
-   */
-  private static Scanner getRow(ScannerOpts scanOpts, Text row) throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
-    // Create a scanner
-    Scanner scanner = connector.createScanner(tableName, Authorizations.EMPTY);
-    scanner.setBatchSize(scanOpts.scanBatchSize);
-    // Say start key is the one with key of row
-    // and end key is the one that immediately follows the row
-    scanner.setRange(new Range(row));
-    return scanner;
-  }
-
 }
