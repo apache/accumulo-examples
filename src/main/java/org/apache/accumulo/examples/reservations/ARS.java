66f5d1c7bcfa2bdc93e9abb575ad483e95eb5870
Jeffrey Manno <jeffreymanno15@gmail.com>
2019-05-08 15:59:44 -0400

    Removes try-block around client. Fixes #45 (#50)

--- src/main/java/org/apache/accumulo/examples/reservations/ARS.java
+++ src/main/java/org/apache/accumulo/examples/reservations/ARS.java
@@ -301,13 +301,15 @@ public class ARS {
       } else if (tokens[0].equals("quit") && tokens.length == 1) {
         break;
       } else if (tokens[0].equals("connect") && tokens.length == 6 && ars == null) {
-        try (AccumuloClient client = Accumulo.newClient().to(tokens[1], tokens[2])
-            .as(tokens[3], tokens[4]).build()) {
-          if (client.tableOperations().exists(tokens[5])) {
-            ars = new ARS(client, tokens[5]);
-            reader.println("  connected");
-          } else
-            reader.println("  No Such Table");
+
+        // the client can't be closed here, because it is passed to the new ARS object
+        AccumuloClient client = Accumulo.newClient().to(tokens[1], tokens[2])
+            .as(tokens[3], tokens[4]).build();
+        if (client.tableOperations().exists(tokens[5])) {
+          ars = new ARS(client, tokens[5]);
+          reader.println("  connected");
+        } else {
+          reader.println("  No Such Table");
         }
       } else {
         System.out.println("  Commands : ");
