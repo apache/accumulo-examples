0869acbf3d7bd49be1e8494e416e2fb68340af9d
Mark Owens <jmark99@apache.org>
2021-04-15 15:29:20 -0400

    Avoid checking Accumulo table exists before creation
    Update log message constants in Constants Enum class.

--- src/main/java/org/apache/accumulo/examples/common/Constants.java
+++ src/main/java/org/apache/accumulo/examples/common/Constants.java
@@ -1,13 +1,11 @@
-package org.apache.accumulo.examples;
+package org.apache.accumulo.examples.common;
 
 public enum Constants {
   ;
-
   public static final String NAMESPACE = "examples";
 
   public static String TABLE_EXISTS_MSG = "Table already exists. User may wish to delete existing "
-      + "table and rerun example. Table name: ";
-
+      + "table and re-run example. Table name: ";
+  public static String NAMESPACE_EXISTS_MSG = "Namespace already exists. Message can be ignored. ";
   public static String ALL_ROWS_SCANNED = "All expected rows were scanned";
-
 }
